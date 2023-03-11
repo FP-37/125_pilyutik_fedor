71ee866f414682c8d200cdd4b61482c807aa24a7
FP-37 <FP-37@yandex.ru>
2023-03-11 14:36:30 +0300

    Random02_Sbornik_Task_3

---
+++ Task_4/Random02_Sbornik_Task_3/Random02.py
@@ -0,0 +1,56 @@
+import random
+
+hihi = []
+haha = []
+
+def neYmejuVTsifri(numbs):
+
+    x = 0
+
+    cheeseburger = random.randint(50, 101)
+    while x < cheeseburger:
+        y = random.randint(0,10000)
+        x+=1
+        numbs.insert(x,y)
+
+    return(numbs)
+
+hihi = neYmejuVTsifri(hihi)
+haha = neYmejuVTsifri(haha)
+
+#Выше – функция заполнения массивов случайной длины случайными числами
+
+def lolProhibitThat(array1,array2):
+
+    x = 0
+
+    while x < len(array1):
+        if array1[x] in array2:
+            array1.remove(array1[x])
+        x = x + 1
+
+    return array1
+
+#Выше – функция удаления из первого массива элементов, встречающихся во втором массиве
+
+lolProhibitThat(hihi, haha)
+
+def sovpadenije(array1, array2):
+
+    massive=[]
+
+    if array1>array2:
+
+        for element in array1:
+            for elmnt in array2:
+                if element == elmnt:
+                    massive.append(element)
+    else:
+
+        for elmnt in array2:
+            for element in array1:
+                if elmnt == element:
+                    massive.append(elmnt)
+    print(massive)
+
+sovpadenije(hihi,haha)
