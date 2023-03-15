e32eacbc219e0d108c8cbe1f1674ae1ab6db8d43
FP-37 <FP-37@yandex.ru>
2023-03-16 02:27:13 +0300

    Files02_Sbornik_Task_16

---
+++ Task_17/Files02_Sbornik_Task_16/Files02.py
@@ -0,0 +1,27 @@
+import datetime
+import time
+import csv
+
+x = 1
+spisok = []
+
+while x <= 300:
+    vremya = datetime.datetime.now()
+    tippaDATA = vremya.strftime('%H:%M %d.%m.%y')
+    tippaSEC = vremya.strftime('%S')
+    tippaMS = vremya.strftime('%f')
+    stroka = [x, tippaDATA, tippaSEC, tippaMS]
+    spisok.append(stroka)
+    x += 1
+    time.sleep(0.01)
+
+file = open('tablitsa.csv', 'w+')
+emae = csv.writer(file,delimiter=';')
+emae.writerows(spisok)
+file.close()
+
+file = open('tablitsa.csv','r')
+emae = csv.reader(file,delimiter=';')
+for row in emae:
+    print(row)
+file.close()
